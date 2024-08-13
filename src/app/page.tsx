0b1841cba731b08fa3a800e2e5e632441a5c6221
Dinesh <dineshigdd@gmail.com>
2024-08-12 22:09:47 -0700

    add onclick event and behavior

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,8 @@ export default  function Home() {
 
   const [ Inventory, setInventory ] = useState<Item[]>([]);
   const listRef = useRef<(HTMLLIElement | null )[]>([]);
-  
+  const [selectedIndex, setSelectedIndex] = useState<number | null>(null); // State to track the selected item
+  const [ isItemselected, setIsItemselected ] = useState(false); 
 
    useEffect(() => {
         (async () => {
@@ -38,23 +39,52 @@ const setRef = (index: number) => (element: HTMLLIElement | null) => {
   listRef.current[index] = element;
 };
    
-const handleMouseOver = ( itemId:number )=> (event: MouseEvent<HTMLLIElement>)=>{
+const handleMouseOver = ( itemId:number )=> (event: MouseEvent<HTMLLIElement>)=> {
     const item = listRef.current[ itemId ];
     
-    if( itemId && item?.style ){
-          item.style.backgroundColor = 'lightgray';
+    if( itemId && item?.style  ){
+          item.style.backgroundColor = 'lightgray';         
     }
-} 
 
-const handleMouseOut = ( itemId:number )=> (event: MouseEvent<HTMLLIElement>)=>{
-  const item = listRef.current[ itemId ];
-  
-  if( itemId && item?.style ){
-        item.style.backgroundColor = '';
+    if( itemId && item?.style && isItemselected ){
+      if( selectedIndex !== itemId ){
+        item.style.backgroundColor = 'lightgray';
+      }else{
+        item.style.backgroundColor = 'gray';
+      }
   }
+
+   
 } 
 
+const handleMouseOut = ( itemId:number )=> (event: MouseEvent<HTMLLIElement>)=> {
+  const item = listRef.current[ itemId ];
+  // setIsItemselected( false )
+  
 
+  if( itemId && item?.style && !isItemselected ) {
+        item.style.backgroundColor = '';      
+  }
+
+  if( itemId && item?.style && isItemselected ){
+    if( selectedIndex !== itemId ){
+      item.style.backgroundColor = '';
+    }else{
+      item.style.backgroundColor = 'gray';
+    }
+}
+} 
+
+const handleClick = (itemId: number) =>(event: MouseEvent<HTMLLIElement>)=> {
+  setSelectedIndex(itemId); // Update the selected item index
+  setIsItemselected( true )
+  
+  listRef.current.forEach((item, idx) => {
+    if (item) {
+      item.style.backgroundColor = idx === itemId ? 'gray' : ''; // Set background to gray for selected item
+    }
+  });
+}; 
 
   return (
     <div className="grid place-items-center min-h-screen bg-blue-100"> {/** <div className="flex items-center justify-center min-h-screen"> */}
@@ -79,6 +109,7 @@ const handleMouseOut = ( itemId:number )=> (event: MouseEvent<HTMLLIElement>)=>{
                             ref={ setRef(item.id)} 
                             onMouseOver={ handleMouseOver( item.id ) } 
                             onMouseOut={ handleMouseOut( item.id )}
+                            onClick={ handleClick(item.id)}
                             key={ item.id }>{ item.name }                            
                         </li>)}
                   </ul>                
